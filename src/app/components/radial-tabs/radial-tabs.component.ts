b5d9cc7ddc5b12806e838ab47f8fcad1c31e7489
FelipeIzRo <felipe.iz.ro.98@gmail.com>
2025-11-06 23:43:18 +0100

    menu de tabs V1

---
+++ src/app/components/radial-tabs/radial-tabs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radial-tabs',
+  imports: [],
+  templateUrl: './radial-tabs.component.html',
+  styleUrl: './radial-tabs.component.css'
+})
+export class RadialTabsComponent {
+
+}
