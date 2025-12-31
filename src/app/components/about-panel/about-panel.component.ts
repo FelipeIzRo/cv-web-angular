e267980c569811d22eeb4572495110b8d6720467
FelipeIzRo <felipe.iz.ro.98@gmail.com>
2025-12-31 10:47:27 +0100

    Creacion de componentes dentrod e about

--- src/app/components/about-panel/about-panel.component.ts
+++ src/app/components/about-panel/about-panel.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../card/card.component";
+import { CommonModule } from '@angular/common';
+import { PersonalInfoComponent } from "../personal-info/personal-info.component";
+import { CurriculumComponent } from "../curriculum/curriculum.component";
 
 @Component({
   selector: 'app-about-panel',
-  imports: [CardComponent],
+  imports: [CommonModule, CardComponent, PersonalInfoComponent, CurriculumComponent],
   templateUrl: './about-panel.component.html',
   styleUrl: './about-panel.component.css'
 })
 export class AboutPanelComponent {
+  content: 'card' | 'personal-info' | 'curriculum' = 'card';
+  title: 'Carta de presentacion' | 'Informacion personal' | 'Curriculum' = 'Carta de presentacion';
 
+  changeContent(){
+    switch(this.content){
+      case 'card': this.title = 'Carta de presentacion'; break;
+      case 'personal-info': this.title = 'Informacion personal'; break;
+      case 'curriculum': this.title = 'Curriculum'; break;
+    }
+  }
 }
