c167e0f64a4c2e3754385e18bcc91f2cb22421aa
FelipeIzRo <felipe.iz.ro.98@gmail.com>
2025-12-31 11:38:51 +0100

    layout de about content para dejar en el centro el componente a un tamaño fijo para cada hijo

--- src/app/components/about-panel/about-panel.component.ts
+++ src/app/components/about-panel/about-panel.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { PersonalInfoComponent } from "../personal-info/personal-info.component";
 import { CurriculumComponent } from "../curriculum/curriculum.component";
 
+type ContentType = 'card' | 'personal-info' | 'curriculum';
+
 @Component({
   selector: 'app-about-panel',
   imports: [CommonModule, CardComponent, PersonalInfoComponent, CurriculumComponent],
@@ -11,7 +13,10 @@ import { CurriculumComponent } from "../curriculum/curriculum.component";
   styleUrl: './about-panel.component.css'
 })
 export class AboutPanelComponent {
-  content: 'card' | 'personal-info' | 'curriculum' = 'card';
+
+  currentIndex = 0;
+  contents: ContentType[] = ['card', 'personal-info', 'curriculum'];
+  content:ContentType = this.contents[this.currentIndex];
   title: 'Carta de presentacion' | 'Informacion personal' | 'Curriculum' = 'Carta de presentacion';
 
   changeContent(){
@@ -19,6 +24,18 @@ export class AboutPanelComponent {
       case 'card': this.title = 'Carta de presentacion'; break;
       case 'personal-info': this.title = 'Informacion personal'; break;
       case 'curriculum': this.title = 'Curriculum'; break;
+    }
+  }
+  next(){
+    if(this.currentIndex < this.contents.length - 1){
+      this.currentIndex++;
+      this.content = this.contents[this.currentIndex];
+    }
+  }
+  previous(){
+    if(this.currentIndex > 0){
+      this.currentIndex--;
+      this.content = this.contents[this.currentIndex];
     }
   }
 }
