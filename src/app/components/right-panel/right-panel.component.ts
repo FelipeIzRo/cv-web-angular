d2f919b5646c58e46164cd01fdf24b59eb15bc33
FelipeIzRo <felipe.iz.ro.98@gmail.com>
2025-12-31 17:59:10 +0100

    Pantalla de contacto creada para redirigir correos con Formspree

--- src/app/components/right-panel/right-panel.component.ts
+++ src/app/components/right-panel/right-panel.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { StudiesPanelComponent } from "../studies-panel/studies-panel.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-right-panel',
-  imports: [CommonModule, StudiesPanelComponent],
+  imports: [CommonModule, StudiesPanelComponent, ContactComponent],
   templateUrl: './right-panel.component.html',
   styleUrl: './right-panel.component.css'
 })
