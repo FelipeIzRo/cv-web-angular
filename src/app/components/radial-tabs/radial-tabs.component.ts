d74ae5c20796c9edb845176c34b923a839a75659
FelipeIzRo <felipe.iz.ro.98@gmail.com>
2026-01-11 18:12:57 +0100

    primera version del "reloj selector de tab"

--- src/app/components/radial-tabs/radial-tabs.component.ts
+++ src/app/components/radial-tabs/radial-tabs.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LeftPanelComponent } from '../left-panel/left-panel.component';
 import { RightPanelComponent } from "../right-panel/right-panel.component";
 
@@ -10,10 +10,55 @@ import { RightPanelComponent } from "../right-panel/right-panel.component";
   templateUrl: './radial-tabs-refactor.component.html',
   styleUrl: './radial-tabs-refactor.component.css'
 })
-export class RadialTabsComponent {
+export class RadialTabsComponent implements OnInit, OnDestroy {
   activeTab: 'about' | 'studies' | 'experience' | 'contact' | null = null;
   focusedTab: 'about' | 'studies' | 'experience' | 'contact' | null = null;
 
+  angle = 0;
+  duration = 26000;
+
+  private start = 0;
+  private rafId!: number;
+
+  ngOnInit() {
+    this.start = performance.now();
+    this.loop();
+  }
+
+  ngOnDestroy() {
+    cancelAnimationFrame(this.rafId);
+  }
+
+  private loop = () => {
+    const now = performance.now();
+    const elapsed = (now - this.start) % this.duration;
+
+    this.angle = (elapsed / this.duration) * 360;
+
+    this.checkMilestones();
+
+    this.rafId = requestAnimationFrame(this.loop);
+  };
+
+  private lastAngle = 0;
+
+  private checkMilestones() {
+    this.checkCross(0, '0 / 360');
+    this.checkCross(90, '90');
+    this.checkCross(180, '180');
+    this.checkCross(270, '270');
+    this.lastAngle = this.angle;
+  }
+
+  private checkCross(target: number, label: string) {
+    if (
+      (this.lastAngle < target && this.angle >= target) ||
+      (target === 0 && this.lastAngle > this.angle)
+    ) {
+      console.log(label);
+    }
+  }
+  
   openTab(tab: 'about' | 'studies' | 'experience' | 'contact') {
     console.log('clicked: ',tab);
     this.activeTab = tab;
